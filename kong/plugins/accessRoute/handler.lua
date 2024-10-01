e368598236f78b1067a52b4818df5dac28189d6b
csermet-ankasoft <csermet@ankasoft.co>
2024-10-01 12:30:24 +0300

    tests deleted

--- kong/plugins/accessRoute/handler.lua
+++ kong/plugins/accessRoute/handler.lua
@@ -3,26 +3,21 @@ local plugin = {
   VERSION = "0.2",
 }
 
-function plugin:init_worker()
-  kong.log.debug("saying hi from the 'init_worker' handler")
-end
-
 function plugin:access(plugin_conf)
+  kong.log.inspect(plugin_conf)
   local headers = kong.request.get_headers()
   local id_header = headers["id"]
 
-  -- Eğer ID "5" ise X servisine yönlendir
   if id_header and id_header == "5" then
-    -- X servisine yönlendir
     kong.service.set_target(plugin_conf.service_x_host, plugin_conf.service_x_port)
   else
-    -- Y servisine yönlendir
     kong.service.set_target(plugin_conf.service_y_host, plugin_conf.service_y_port)
   end
+  kong.service.request.set_header(plugin_conf.request_header, "this is on a request")
 end
 
 function plugin:header_filter(plugin_conf)
-  kong.response.set_header(plugin_conf.response_header, "this is on the response")
+  kong.response.set_header(plugin_conf.response_header, "response Test")
 end
 
 return plugin
--- kong/plugins/accessRoute/schema.lua
+++ kong/plugins/accessRoute/schema.lua
@@ -10,17 +10,6 @@ local schema = {
     { config = {
         type = "record",
         fields = {
-          { request_header = typedefs.header_name {
-              required = true,
-              default = "Hello-World" } },
-          { response_header = typedefs.header_name {
-              required = true,
-              default = "Bye-World" } },
-          { ttl = {
-              type = "integer",
-              default = 600,
-              required = true,
-              gt = 0, }},
           { service_x_host = {
               type = "string",
               required = true,
@@ -36,7 +25,8 @@ local schema = {
           { service_y_port = {
               type = "integer",
               required = true,
-              default = 80 }},
+              default = 80 }
+            },
         },
       },
     },
